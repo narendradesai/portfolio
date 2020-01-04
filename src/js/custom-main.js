cafd69ca56a00a5854c7d19ca32b1742e9bf33f5
apple <apple@apples-MacBook-Pro.local>
2020-01-05 01:18:32 +0530

    initial commit

---
+++ src/js/custom-main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // $("body").jpreLoader({
+  //   preMainSection: "#main-preloader",
+  //   prePerText: ".preloader-percentage-text",
+  //   preBar: ".preloader-bar"
+  // });
+  // console.log('sfsf');
+  var body = $('body');
+
+  $('nav .navbar-toggle').on('click', function() {
+    $('nav .navbar-content').slideToggle();
+    $(this).find('span').toggleClass('fa-times');
+    $(this).find('span').toggleClass('fa-bars');
+  });
+  // var item = document.getElementsByClassName('nav-wrapper')[0];
+  // var parallaxInstance = new Parallax(item, {
+  //   relativeInput: true
+  // });
+  parallaxInstance.friction(0.2, 0.2);
+});
