5bb5604b257d154f9da0b35ec6d8f8458be7e3d4
apple <apple@apples-MacBook-Pro.local>
2020-01-08 10:45:57 +0530

    js changes home page nav bar

--- src/js/custom-main.js
+++ src/js/custom-main.js
@@ -6,12 +6,22 @@ $(document).ready(function() {
   // });
   // console.log('sfsf');
   var body = $('body');
+  var $win = $(window);
+  var winH = $win.height();  
 
   $('nav .navbar-toggle').on('click', function() {
     $('nav .navbar-content').slideToggle();
     $(this).find('span').toggleClass('fa-times');
     $(this).find('span').toggleClass('fa-bars');
   });
+  // .slideToggle($(this).scrollTop() > winH );
+  var nav = $('nav.top-fixed-nav');
+  $win.on("scroll", function () {
+    nav.slideToggle($(this).scrollTop() > winH );
+});
+// .on("resize", function(){ // If the user resizes the window
+//    winH = $(this).height(); // you'll need the new height value
+// });
   // var item = document.getElementsByClassName('nav-wrapper')[0];
   // var parallaxInstance = new Parallax(item, {
   //   relativeInput: true
