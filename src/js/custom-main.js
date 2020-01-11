fdbc2c5f073d32b33fb0ee9ac645b90a52d21778
apple <apple@apples-MacBook-Pro.local>
2020-01-12 00:56:22 +0530

    Scroll navigation css and js updated

--- src/js/custom-main.js
+++ src/js/custom-main.js
@@ -5,26 +5,36 @@ $(document).ready(function() {
   //   preBar: ".preloader-bar"
   // });
   // console.log('sfsf');
-  var body = $('body');
   var $win = $(window);
-  var winH = $win.height();  
+  var winH = $win.height() - 20;
 
-  $('nav .navbar-toggle').on('click', function() {
-    $('nav .navbar-content').slideToggle();
-    $(this).find('span').toggleClass('fa-times');
-    $(this).find('span').toggleClass('fa-bars');
+  $("nav .navbar-toggle").on("click", function() {
+    $("nav .navbar-content").slideToggle();
+    $(this)
+      .find("span")
+      .toggleClass("fa-times");
+    $(this)
+      .find("span")
+      .toggleClass("fa-bars");
   });
-  // .slideToggle($(this).scrollTop() > winH );
-  var nav = $('nav.top-fixed-nav');
-  $win.on("scroll", function () {
-    nav.slideToggle($(this).scrollTop() > winH );
-});
-// .on("resize", function(){ // If the user resizes the window
-//    winH = $(this).height(); // you'll need the new height value
-// });
-  // var item = document.getElementsByClassName('nav-wrapper')[0];
-  // var parallaxInstance = new Parallax(item, {
-  //   relativeInput: true
-  // });
-  // parallaxInstance.friction(0.2, 0.2);
+  var nav = $("nav.top-fixed-nav");
+  $win.on("scroll", function() {
+    var scrollPos = $(document).scrollTop();
+    $(this).scrollTop() > winH
+      ? nav.removeClass("hide-nav")
+      : nav.addClass("hide-nav");
+    $("nav.top-fixed-nav .nav-link").each(function() {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (
+        refElement.position().top <= scrollPos &&
+        refElement.position().top + refElement.height() > scrollPos
+      ) {
+        $("nav.top-fixed-nav .nav-item").removeClass("active");
+        currLink.parents(".nav-item").addClass("active");
+      } else {
+        currLink.parents(".nav-item").removeClass("active");
+      }
+    });
+  });
 });
